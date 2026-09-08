02dd0e88fdb07433b7be8f2bcc074a3582005e12
Laih <laila.ihler@gmail.com>
2026-09-08 19:42:49 -0300

    sumo la clase imagen

---
+++ TPWinForm_equipo-5/Dominio/Imagen.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPWinForm_equipo_5.Dominio
+{
+    internal class Imagen
+    {
+        public int Id { get; set;}
+        public int IdArticulo { get; set; }
+        public string ImagenURL { get; set; }
+    }
+}
