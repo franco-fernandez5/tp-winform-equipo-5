ecda4e91987c321ababcb83d0ae980b3d398aca5
Franco Fernandez <francofernandez0514@gmail.com>
2026-09-10 23:14:51 -0300

    Diseño de frmMarcas (Botones | Nueva Marca - Modificar Marca - Eliminar Marca)

---
+++ TPWinForm_equipo-5/Formularios/FrmMarcas.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace TPWinForm_equipo_5.Formularios
+{
+    partial class FrmMarcas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNuevaMarca = new System.Windows.Forms.Button();
+            this.btnModificarMarca = new System.Windows.Forms.Button();
+            this.btnEliminarMarca = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnNuevaMarca
+            // 
+            this.btnNuevaMarca.Location = new System.Drawing.Point(116, 320);
+            this.btnNuevaMarca.Name = "btnNuevaMarca";
+            this.btnNuevaMarca.Size = new System.Drawing.Size(159, 72);
+            this.btnNuevaMarca.TabIndex = 0;
+            this.btnNuevaMarca.Text = "Nurva Marca";
+            this.btnNuevaMarca.UseVisualStyleBackColor = true;
+            this.btnNuevaMarca.Click += new System.EventHandler(this.btnNuevaMarca_Click);
+            // 
+            // btnModificarMarca
+            // 
+            this.btnModificarMarca.Location = new System.Drawing.Point(299, 320);
+            this.btnModificarMarca.Name = "btnModificarMarca";
+            this.btnModificarMarca.Size = new System.Drawing.Size(156, 72);
+            this.btnModificarMarca.TabIndex = 1;
+            this.btnModificarMarca.Text = "Modificar Marca";
+            this.btnModificarMarca.UseVisualStyleBackColor = true;
+            this.btnModificarMarca.Click += new System.EventHandler(this.btnModificarMarca_Click);
+            // 
+            // btnEliminarMarca
+            // 
+            this.btnEliminarMarca.Location = new System.Drawing.Point(477, 320);
+            this.btnEliminarMarca.Name = "btnEliminarMarca";
+            this.btnEliminarMarca.Size = new System.Drawing.Size(167, 72);
+            this.btnEliminarMarca.TabIndex = 2;
+            this.btnEliminarMarca.Text = "EliminarMarca";
+            this.btnEliminarMarca.UseVisualStyleBackColor = true;
+            this.btnEliminarMarca.Click += new System.EventHandler(this.btnEliminarMarca_Click);
+            // 
+            // FrmMarcas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnEliminarMarca);
+            this.Controls.Add(this.btnModificarMarca);
+            this.Controls.Add(this.btnNuevaMarca);
+            this.Name = "FrmMarcas";
+            this.Text = "FrmMarcas";
+            this.Load += new System.EventHandler(this.FrmMarcas_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNuevaMarca;
+        private System.Windows.Forms.Button btnModificarMarca;
+        private System.Windows.Forms.Button btnEliminarMarca;
+    }
+}
---
+++ TPWinForm_equipo-5/Formularios/FrmMarcas.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TPWinForm_equipo_5.Formularios
+{
+    public partial class FrmMarcas : Form
+    {
+        public FrmMarcas()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMarcas_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNuevaMarca_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnModificarMarca_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEliminarMarca_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
