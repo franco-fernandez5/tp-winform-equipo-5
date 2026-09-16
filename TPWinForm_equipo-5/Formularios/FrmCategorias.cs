58add0e516f00436cc64ec4b70c2af5902353a46
GabrielMGaber <gabrielgaber123@gmail.com>
2026-09-16 18:16:45 -0300

    Listado de Categorias

--- TPWinForm_equipo-5/Formularios/FrmCategorias.cs
+++ TPWinForm_equipo-5/Formularios/FrmCategorias.cs
@@ -8,11 +8,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using TPWinForm_equipo_5.Dominio;
+using TPWinForm_equipo_5.Negocio;
 
 namespace TPWinForm_equipo_5.Formularios
 {
     public partial class FrmCategorias : Form
     {
+
+        private List<Categoria> listaCategorias;
         public FrmCategorias()
         {
             InitializeComponent();
@@ -20,13 +23,35 @@ namespace TPWinForm_equipo_5.Formularios
 
         private void FrmCategorias_Load(object sender, EventArgs e)
         {
-
+            cargar();
         }
+
+
+        private void cargar()
+        {
+            CategoriaNegocio negocio = new CategoriaNegocio();
+
+            try
+            {
+                listaCategorias = negocio.listar();
+                dgvCategorias.DataSource = listaCategorias;
+                dgvCategorias.Columns["Id"].Visible = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+
+
+
 
         private void btnNuevaCategoria_Click(object sender, EventArgs e)
         {
             frmAgregarModificarCategoria form = new frmAgregarModificarCategoria();   
             form.ShowDialog();
+            cargar();
         }
 
         private void btnModificarCategoria_Click(object sender, EventArgs e)
@@ -40,6 +65,7 @@ namespace TPWinForm_equipo_5.Formularios
             Categoria seleccionada = (Categoria)dgvCategorias.CurrentRow.DataBoundItem;
             frmAgregarModificarCategoria form = new frmAgregarModificarCategoria(seleccionada);   
             form.ShowDialog();
+            cargar();
         }
 
         private void btnEliminarCategoria_Click(object sender, EventArgs e)
@@ -61,8 +87,9 @@ namespace TPWinForm_equipo_5.Formularios
 
             if (respuesta == DialogResult.Yes)
             {
-     
-               
+                MessageBox.Show("Eliminado ");
+                cargar();
+
             }
 
         }
